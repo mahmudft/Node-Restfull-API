6db2447286702cca78d72c652f3ed75ec80b20e2
Mahmud <fatullayevm@gmail.com>
2021-06-24 17:55:23 +0400

    Rechange

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify({ message: 'No Route' }));
     }
 });
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true }).then(res => server.listen(port, () => {
+mongoose.connect('mongodb://db:27017/test', { useNewUrlParser: true, useUnifiedTopology: true }).then(res => server.listen(port, () => {
     console.log(`Server running on PORT: ${port}`);
 })).catch(err => console.log(err));
 
